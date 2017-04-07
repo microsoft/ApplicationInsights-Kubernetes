7414333828959e9be28f206c8a1c2fbb1170ca71
Saar Shen <saars@microsoft.com>
2017-04-07 15:27:30 -0700

    Add replicaSet to telemetry initializer

---
+++ src/TelemetryInitializers/Prefixes.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.ApplicationInsights.Netcore.Kubernetes.TelemetryInitializers
+{
+    internal class Prefixes
+    {
+        public const string K8s = "K8s";
+        public const string Container = "Container";
+        public const string Pod = "Pod";
+        public const string ReplicaSet = "ReplicaSet";
+    }
+}
