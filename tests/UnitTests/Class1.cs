8e05456c037e16e9f4ab53f2ec2fc78870e7c117
Saar Shen <saars@microsoft.com>
2017-04-11 13:37:19 -0700

    Add unit tests project

---
+++ tests/UnitTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTests
+{
+    using Xunit;
+
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.Equal(4, 5);
+        }
+    }
+}
