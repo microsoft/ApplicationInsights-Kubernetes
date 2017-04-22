db5883ffeba53f5ba7ffebad9b027372692ab93b
Saar Shen <saars@microsoft.com>
2017-04-21 18:55:09 -0700

    More protection for K8sEnvironement initializer

--- src/Extensions/ApplicationInsightsExtensions.cs
+++ src/Extensions/ApplicationInsightsExtensions.cs
@@ -16,14 +16,29 @@
             // 2 minutes maximum to spin up the container.
             timeout = timeout ?? TimeSpan.FromMinutes(2);
             ILoggerFactory loggerFactory = (ILoggerFactory)services.FirstOrDefault(s => s.ServiceType == typeof(ILoggerFactory))?.ImplementationInstance;
+            ILogger logger = loggerFactory?.CreateLogger("K8sEnvInitializer");
 
-            K8sEnvironment k8sEnv = K8sEnvironment.CreateAsync(timeout.Value, loggerFactory).ConfigureAwait(false).GetAwaiter().GetResult();
-            // Wait until the initialization is done.
-            k8sEnv.InitializationWaiter.WaitOne(TimeSpan.FromMinutes(1));
+            try
+            {
+                K8sEnvironment k8sEnv = K8sEnvironment.CreateAsync(timeout.Value, loggerFactory).ConfigureAwait(false).GetAwaiter().GetResult();
+                if (k8sEnv != null)
+                {
+                    // Wait until the initialization is done.
+                    k8sEnv.InitializationWaiter.WaitOne(TimeSpan.FromMinutes(1));
 
-            // Inject the telemetry initializer.
-            services.AddSingleton<IK8sEnvironment>(k8sEnv);
-            services.AddSingleton<ITelemetryInitializer, KubernetesTelemetryInitializer>();
+                    // Inject the telemetry initializer.
+                    services.AddSingleton<IK8sEnvironment>(k8sEnv);
+                    services.AddSingleton<ITelemetryInitializer, KubernetesTelemetryInitializer>();
+                }
+                else
+                {
+                    logger?.LogWarning("Application Insights Kubernetes failed to start.");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+            }
             return services;
         }
     }
