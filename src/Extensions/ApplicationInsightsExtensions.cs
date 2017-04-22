8e8eab02f15ec310c14bdd1b02b74858b9ca9932
Saar Shen <saars@microsoft.com>
2017-04-21 18:40:40 -0700

    Update the way to fetch ILoggerFactory

--- src/Extensions/ApplicationInsightsExtensions.cs
+++ src/Extensions/ApplicationInsightsExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Microsoft.Extensions.DependencyInjection
 {
     using System;
+    using System.Linq;
     using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.ApplicationInsights.Kubernetes;
     using Microsoft.Extensions.Logging;
@@ -10,10 +11,12 @@
     /// </summary>
     public static class ApplicationInsightsExtensions
     {
-        public static IServiceCollection EnableK8s(this IServiceCollection services, TimeSpan? timeout = null, ILoggerFactory loggerFactory = null)
+        public static IServiceCollection EnableK8s(this IServiceCollection services, TimeSpan? timeout = null)
         {
             // 2 minutes maximum to spin up the container.
             timeout = timeout ?? TimeSpan.FromMinutes(2);
+            ILoggerFactory loggerFactory = (ILoggerFactory)services.FirstOrDefault(s => s.ServiceType == typeof(ILoggerFactory))?.ImplementationInstance;
+
             K8sEnvironment k8sEnv = K8sEnvironment.CreateAsync(timeout.Value, loggerFactory).ConfigureAwait(false).GetAwaiter().GetResult();
             // Wait until the initialization is done.
             k8sEnv.InitializationWaiter.WaitOne(TimeSpan.FromMinutes(1));
