08ad88104de5b8e93b10771980f858a8af1cfcb3
Saar Shen <saars@microsoft.com>
2018-02-05 11:41:52 -0800

    Implement IHostingStartup to support ASP.NET Core 2.0 DependencyInjection feature light up (#88)
    * Relocate source files for more than 1 project in sln
    
    * Update Moq
    
    * Hosting startup implementations
    
    * Share more build properties
    
    * Tweak name spaces
    
    * Address a bad merge

---
+++ src/ApplicationInsights.Kubernetes/Extensions/HostingStartup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ApplicationInsights.Kubernetes.Extensions
+{
+    public class HostingStartup
+    {
+
+    }
+}
