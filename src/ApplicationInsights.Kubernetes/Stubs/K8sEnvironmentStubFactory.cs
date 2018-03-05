7aeeefc727462d048069a565ac828770937d0a40
Saar Shen <xiaomi7732@hotmail.com>
2018-03-04 18:22:44 -0800

    Add some stub data

--- src/ApplicationInsights.Kubernetes/Stubs/K8sEnvironmentStubFactory.cs
+++ src/ApplicationInsights.Kubernetes/Stubs/K8sEnvironmentStubFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Kubernetes.Entities;
 
 namespace Microsoft.ApplicationInsights.Kubernetes.Stubs
 {
@@ -9,7 +11,59 @@ namespace Microsoft.ApplicationInsights.Kubernetes.Stubs
         {
             return Task.FromResult(new K8sEnvironment()
             {
-                ContainerID = KubeHttpClientSettingsStub.FakeContainerId
+                ContainerID = KubeHttpClientSettingsStub.FakeContainerId,
+                myContainerStatus = new ContainerStatus()
+                {
+                    ContainerID = KubeHttpClientSettingsStub.FakeContainerId,
+                    Image = nameof(ContainerStatus.Image),
+                    ImageID = nameof(ContainerStatus.ImageID),
+                    Name = nameof(ContainerStatus.Name),
+                    Ready = true,
+                },
+                myDeployment = new K8sDeployment()
+                {
+                    Metadata = new K8sDeploymentMetadata()
+                    {
+                        Labels = new Dictionary<string, string>() { { "app", "stub" } },
+                        Name = nameof(K8sDeploymentMetadata.Name),
+                        Uid = nameof(K8sDeploymentMetadata.Uid),
+                    },
+                    Spec = new K8sDeploymentSpec()
+                    {
+                        Selector = new Selector()
+                        {
+                            MatchLabels = new Dictionary<string, string>() { { "app", "stub" } },
+                        },
+                    },
+                },
+                myNode = new K8sNode()
+                {
+                    Metadata = new K8sNodeMetadata()
+                    {
+                        Labels = new Dictionary<string, string>() { { "app", "stub" } },
+                        Name = nameof(K8sNodeMetadata.Name),
+                        Uid = nameof(K8sNodeMetadata.Uid),
+                    },
+                    Status = new K8sNodeStatus()
+                    {
+                    },
+                },
+                myPod = new K8sPod()
+                {
+                    Metadata = new K8sPodMetadata()
+                    {
+                        Uid = "StubPodId",
+                        Name = "StubPodName",
+                        Labels = new Dictionary<string, string>() { { "app", "stub" } },
+                    }
+                },
+                myReplicaSet = new K8sReplicaSet()
+                {
+                    Metadata = new K8sReplicaSetMetadata()
+                    {
+                        Name = "StubReplicaName",
+                    }
+                }
             });
         }
     }
