bb60da136119b8945d3fc02b3efc196209cb1b8c
Saar Shen <xiaomi7732@hotmail.com>
2018-04-16 17:23:28 -0700

    Enable AI for K8s

--- examples/BasicUsage/Program.cs
+++ examples/BasicUsage/Program.cs
@@ -19,6 +19,7 @@ namespace BasicUsage
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
