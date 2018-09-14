bb6d1ebe4f2a2b902c10e426c968405bf6bc9308
Saar Shen <xiaomi7732@hotmail.com>
2018-09-13 20:37:55 -0700

    Barebone for the example

---
+++ examples/HostBuilder/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostBuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
