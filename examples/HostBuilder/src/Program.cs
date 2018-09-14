7c9b28e4233cc0e64425dde953805708bf9509d9
Saar Shen <xiaomi7732@hotmail.com>
2018-09-13 21:06:52 -0700

    Hosted service running

--- examples/HostBuilder/src/Program.cs
+++ examples/HostBuilder/src/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HostBuilderExample
 {
@@ -6,7 +11,16 @@ namespace HostBuilderExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new HostBuilder().ConfigureAppConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                }).ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IHostedService, PrintHelloService>();
+                }).UseConsoleLifetime().Build();
+
+            host.Run();
         }
     }
 }
