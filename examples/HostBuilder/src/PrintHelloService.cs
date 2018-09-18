651bd2ce0fbb6bf0e6027a18cb8b3c5db16291cc
Saar Shen <xiaomi7732@hotmail.com>
2018-09-17 18:16:32 -0700

    WIP: Hosting Startup

--- examples/HostBuilder/src/PrintHelloService.cs
+++ examples/HostBuilder/src/PrintHelloService.cs
@@ -1,14 +1,22 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.Extensions.Hosting;
 
 namespace HostBuilderExample
 {
     public class PrintHelloService : IHostedService
     {
+        TelemetryClient _client;
+        public PrintHelloService(TelemetryClient telemetryClient)
+        {
+            this._client = telemetryClient;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
+
             Console.WriteLine("Hello Hosted Service!!");
             return Task.CompletedTask;
         }
--- examples/HostBuilder/src/Program.cs
+++ examples/HostBuilder/src/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.ApplicationInsights;
 
 namespace HostBuilderExample
 {
@@ -11,14 +12,24 @@ namespace HostBuilderExample
     {
         static void Main(string[] args)
         {
-            var host = new HostBuilder().ConfigureAppConfiguration(configHost =>
+            var host = new HostBuilder().ConfigureHostConfiguration(configHost =>
                 {
                     configHost.SetBasePath(Directory.GetCurrentDirectory());
                     configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables();
                 }).ConfigureServices((hostContext, services) =>
                 {
+                    services.AddApplicationInsightsTelemetry(options =>{
+                        options.InstrumentationKey="9558b686-2a23-441c-b389-37b04312c4ad";
+                        options.
+                    });
+                    // Enable Application Insights with iKey configured in the environemnt variable
                     services.AddSingleton<IHostedService, PrintHelloService>();
-                }).UseConsoleLifetime().Build();
+                    // Enable Application Insights for Kubernetes when running inside Kubernetes.
+                    services.EnableKubernetes();
+                })
+                .UseConsoleLifetime()
+                .Build();
 
             host.Run();
         }
