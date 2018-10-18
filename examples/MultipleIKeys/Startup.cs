ceda77815048a509df538cf2353f39053f3ca3df
Saar Shen <saars@microsoft.com>
2018-10-18 12:35:41 -0700

    Update package versions (#143)
    1. Address vulnerability issues by bumping up external package versions
    in the examples.
    2. Bump up the references to the NuGet package of AI.K8S itself to the
    latested non-prerelease version.

--- examples/MultipleIKeys/Startup.cs
+++ examples/MultipleIKeys/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
