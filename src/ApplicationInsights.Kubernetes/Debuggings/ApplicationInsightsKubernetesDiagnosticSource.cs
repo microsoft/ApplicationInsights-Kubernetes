f67e055668b8d109b5d7ab88e84b35f2e4d56a80
Saar Shen <xiaomi7732@hotmail.com>
2019-06-03 15:45:31 -0700

    Complete the diagnostic listner

--- src/ApplicationInsights.Kubernetes/Debuggings/ApplicationInsightsKubernetesDiagnosticSource.cs
+++ src/ApplicationInsights.Kubernetes/Debuggings/ApplicationInsightsKubernetesDiagnosticSource.cs
@@ -3,66 +3,135 @@ using System.Globalization;
 
 namespace Microsoft.ApplicationInsights.Kubernetes.Debugging
 {
-    internal sealed class ApplicationInsightsKubernetesDiagnosticSource
+    /// <summary>
+    /// Diagnostic Source of Application Insights for Kubernetes.
+    /// </summary>
+    public sealed class ApplicationInsightsKubernetesDiagnosticSource
     {
-        private DiagnosticSource _inner;
+        private DiagnosticSource _innerSource { get; }
+
+        /// <summary>
+        /// Diagnostic source listener subscribe point.
+        /// </summary>
+        /// <returns></returns>
+        public DiagnosticListener Observer => (DiagnosticListener)this._innerSource;
+
+        /// <summary>
+        /// Gets the name of the diagnostic source.
+        /// </summary>
         public const string DiagnosticSourceName = "ApplicationInsightsKubernetesDiagnosticSource";
 
         private ApplicationInsightsKubernetesDiagnosticSource()
         {
-            _inner = new DiagnosticListener(DiagnosticSourceName);
+            _innerSource = new DiagnosticListener(DiagnosticSourceName);
         }
+
+        /// <summary>
+        /// Gets the singleton instance of Application Insights for Kubernetes.
+        /// </summary>
+        /// <returns></returns>
         public static ApplicationInsightsKubernetesDiagnosticSource Instance { get; } = new ApplicationInsightsKubernetesDiagnosticSource();
 
+        /// <summary>
+        /// Logs the critical message.
+        /// </summary>
         public void LogCritical(string message, params object[] args)
         {
             Write(Level.Critical, message, args);
         }
 
+        /// <summary>
+        /// Logs the error.
+        /// </summary>
         public void LogError(string message, params object[] args)
         {
             Write(Level.Error, message, args);
         }
 
+        /// <summary>
+        /// Logs the warning message.
+        /// </summary>
         public void LogWarning(string message, params object[] args)
         {
             Write(Level.Warning, message, args);
         }
 
+        /// <summary>
+        /// Logs the information.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public void LogInformation(string message, params object[] args)
         {
             Write(Level.Information, message, args);
         }
 
+        /// <summary>
+        /// Logs the debugging message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public void LogDebug(string message, params object[] args)
         {
             Write(Level.Debug, message, args);
         }
 
+        /// <summary>
+        /// Logs the trace message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public void LogTrace(string message, params object[] args)
         {
             Write(Level.Trace, message, args);
         }
 
+        /// <summary>
+        /// Diagnostic Source message levels
+        /// </summary>
+        public static class Level
+        {
+            /// <summary>
+            /// Gets the level of critical.
+            /// </summary>
+            /// <returns></returns>
+            public const string Critical = nameof(Critical);
+            /// <summary>
+            /// Gets the level of error.
+            /// </summary>
+            /// <returns></returns>
+            public const string Error = nameof(Error);
+            /// <summary>
+            /// Gets the level of warning.
+            /// </summary>
+            /// <returns></returns>
+            public const string Warning = nameof(Warning);
+            /// <summary>
+            /// Gets the level of information.
+            /// </summary>
+            /// <returns></returns>
+            public const string Information = nameof(Information);
+            /// <summary>
+            /// Gets the level of debug.
+            /// </summary>
+            /// <returns></returns>
+            public const string Debug = nameof(Debug);
+            /// <summary>
+            /// Gets the level of trace.
+            /// </summary>
+            /// <returns></returns>
+            public const string Trace = nameof(Trace);
+        }
+
         private void Write(string level, string message, params object[] args)
         {
-            if (_inner.IsEnabled(DiagnosticSourceName + "." + level))
+            if (_innerSource.IsEnabled(level))
             {
-                _inner.Write(level, new
+                _innerSource.Write(level, new
                 {
                     content = string.Format(CultureInfo.InvariantCulture, message, args),
                 });
             }
-        }
-
-        public static class Level
-        {
-            public const string Critical = nameof(Critical);
-            public const string Error = nameof(Error);
-            public const string Warning = nameof(Warning);
-            public const string Information = nameof(Information);
-            public const string Debug = nameof(Debug);
-            public const string Trace = nameof(Trace);
         }
     }
 }
--- src/ApplicationInsights.Kubernetes/Debuggings/ApplicationInsightsKubernetesDiagnosticSourceListener.cs
+++ src/ApplicationInsights.Kubernetes/Debuggings/ApplicationInsightsKubernetesDiagnosticSourceListener.cs
@@ -2,12 +2,66 @@ using Microsoft.Extensions.DiagnosticAdapter;
 
 namespace Microsoft.ApplicationInsights.Kubernetes.Debugging
 {
+    /// <summary>
+    /// The default listener of Application Insights for Kubernetes.
+    /// </summary>
     public class ApplicationInsightsKubernetesDiagnosticSourceConsoleListener
     {
-        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.DiagnosticSourceName + "." + ApplicationInsightsKubernetesDiagnosticSource.Level.Information)]
+        /// <summary>
+        /// Invokes the message of critical.
+        /// </summary>
+        /// <param name="content"></param>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Critical)]
+        public void OnLogCritical(string content)
+        {
+            WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Critical, content);
+        }
+
+        /// <summary>
+        /// Invokes the message of error.
+        /// </summary>
+        /// <param name="content"></param>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Error)]
+        public void OnLogError(string content)
+        {
+            WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Error, content);
+        }
+
+        /// <summary>
+        /// Invokes the message of warning.
+        /// </summary>
+        /// <param name="content"></param>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Warning)]
+        public void OnLogWarning(string content)
+        {
+            WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Warning, content);
+        }
+
+        /// <summary>
+        /// Invokes the message of information.
+        /// </summary>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Information)]
         public void OnLogInfo(string content)
         {
             WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Information, content);
+        }
+
+        /// <summary>
+        /// Invokes the message of debugging.
+        /// </summary>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Debug)]
+        public void OnLogDebug(string content)
+        {
+            WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Debug, content);
+        }
+
+        /// <summary>
+        /// Invokes the message of tracing.
+        /// </summary>
+        [DiagnosticName(ApplicationInsightsKubernetesDiagnosticSource.Level.Trace)]
+        public void OnLogTrace(string content)
+        {
+            WriteLine(ApplicationInsightsKubernetesDiagnosticSource.Level.Trace, content);
         }
 
         private void WriteLine(string level, string content)
