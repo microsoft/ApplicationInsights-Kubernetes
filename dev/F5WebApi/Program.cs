71b2fa3ad87cbe6aa2e629f54390d928727303f6
Saar Shen <saars@microsoft.com>
2019-06-07 15:52:11 -0700

    Update F5 WebApi project

--- dev/F5WebApi/Program.cs
+++ dev/F5WebApi/Program.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace F5WebApi
 {
@@ -18,11 +10,8 @@ namespace F5WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
--- dev/F5WebApi/Startup.cs
+++ dev/F5WebApi/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using Microsoft.ApplicationInsights.Kubernetes.Debugging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace F5WebApi
 {
@@ -28,21 +21,26 @@ namespace F5WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             // To use this project for F5 debugging, follow these steps:
+            
+            // Output the diagnostic source logs to the console.
+            var observer = new ApplicationInsightsKubernetesDiagnosticObserver(DiagnosticLogLevel.Trace);
+            ApplicationInsightsKubernetesDiagnosticSource.Instance.Observable.SubscribeWithAdapter(observer);
+
             // Step 1. Set iKey in the parameter below.
             services.AddApplicationInsightsTelemetry("your-instrumentation-key");
-            
+
             // Step 2. Call proper overloads of AddApplicationInsightsKubernetesEnricher.
             services.AddApplicationInsightsKubernetesEnricher(applyOptions: null,
-                kubernetesServiceCollectionBuilder: KubernetesDebuggingServiceCollectionBuilderFactory.Instance.Create(null),
-                detectKubernetes: () => true,
-                null);
+                kubernetesServiceCollectionBuilder: KubernetesDebuggingServiceCollectionBuilderFactory.Instance.Create(),
+                detectKubernetes: () => true);
             // Step 3. Set a break point and press F5.
             // ~
-            services.AddMvc().AddNewtonsoftJson();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -55,13 +53,7 @@ namespace F5WebApi
             }
 
             app.UseHttpsRedirection();
-
-            app.UseRouting(routes =>
-            {
-                routes.MapControllers();
-            });
-
-            app.UseAuthorization();
+            app.UseMvc();
         }
     }
 }
