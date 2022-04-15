190af3ca48a1dfb3bb67f19e44bd373bbe041488
Saar Shen <xiaomi7732@hotmail.com>
2022-04-15 10:14:21 -0700

    Initialize the example

---
+++ examples/WorkerExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApplicationInsights.Kubernetes.Examples;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.AddHostedService<Worker>();
+            });
+}
