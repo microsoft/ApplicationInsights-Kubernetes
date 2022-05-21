5d4722bfee72e97ea37d10e01ed82fa8ea34d826
Saar Shen <xiaomi7732@hotmail.com>
2022-05-20 18:05:14 -0700

    Upgrade dev project

--- dev/F5WebApi/Program.cs
+++ dev/F5WebApi/Program.cs
@@ -1,17 +1,38 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.ApplicationInsights.Kubernetes.Debugging;
 
-namespace F5WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Output the diagnostic source logs to the console.
+var observer = new ApplicationInsightsKubernetesDiagnosticObserver(DiagnosticLogLevel.Trace);
+ApplicationInsightsKubernetesDiagnosticSource.Instance.Observable.SubscribeWithAdapter(observer);
+
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddApplicationInsightsKubernetesEnricher();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
