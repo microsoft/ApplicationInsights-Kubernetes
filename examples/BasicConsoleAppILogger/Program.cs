4728a91de7fab97d702782ceedca06fde8f81590
Saar Shen <xiaomi7732@hotmail.com>
2022-05-23 09:48:49 -0700

    Dev/saars/update console example (#260)
    * Upgrade the exmaple for ilogger to .NET 6
    
    * Bump up dependencies for examples
    
    * Comment out self-diagnostic code for ilogger example

--- examples/BasicConsoleAppILogger/Program.cs
+++ examples/BasicConsoleAppILogger/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿// using System.Diagnostics;
+// using Microsoft.ApplicationInsights.Kubernetes.Debugging;
+
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.ApplicationInsights.Kubernetes.Debugging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace BasicConsoleAppILogger
 {
@@ -16,20 +19,26 @@ namespace BasicConsoleAppILogger
         {
             // Create the DI container.
             IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(p => new ConfigurationBuilder().Build());
 
             // Uncomment the following lines for debugging AI.K8s.
             // Refer to https://github.com/microsoft/ApplicationInsights-Kubernetes/blob/develop/docs/SelfDiagnostics.MD for details.
             // var observer = new ApplicationInsightsKubernetesDiagnosticObserver(DiagnosticLogLevel.Trace);
             // ApplicationInsightsKubernetesDiagnosticSource.Instance.Observable.SubscribeWithAdapter(observer);
 
-            // Add application insights for Kubernetes. Making sure this is called before services.Configure<TelemetryConfiguration>().
             services.AddApplicationInsightsKubernetesEnricher();
 
             // Channel is explicitly configured to do flush on it later.
             var channel = new InMemoryChannel();
-            services.Configure<TelemetryConfiguration>(
-                (config) =>
+            services.AddOptions<TelemetryConfiguration>().Configure<IServiceProvider>((config, p) =>
                 {
+                    // Appending registered telemetry initializers.
+                    foreach (ITelemetryInitializer registeredInitializer in p.GetServices<ITelemetryInitializer>())
+                    {
+                        config.TelemetryInitializers.Add(registeredInitializer);
+                    }
+
+                    // Setup up telemetry channels.
                     config.TelemetryChannel = channel;
                 }
             );
@@ -43,7 +52,11 @@ namespace BasicConsoleAppILogger
                 builder.AddApplicationInsights("---Your AI instrumentation key---");
 
                 // Optional: Show the logs in console at the same time
-                builder.AddConsole();
+                builder.AddSimpleConsole(opt =>
+                {
+                    opt.SingleLine = true;
+                    opt.ColorBehavior = LoggerColorBehavior.Disabled;
+                });
             });
 
             // Build ServiceProvider.
--- examples/WorkerExample/Program.cs
+++ examples/WorkerExample/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// using System.Diagnostics;
+// using Microsoft.ApplicationInsights.Kubernetes.Debugging;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace ApplicationInsights.Kubernetes.Examples;
@@ -15,6 +17,11 @@ class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureServices((hostContext, services) =>
             {
+                // Uncomment the following lines for debugging AI.K8s.
+                // Refer to https://github.com/microsoft/ApplicationInsights-Kubernetes/blob/develop/docs/SelfDiagnostics.MD for details.
+                // var observer = new ApplicationInsightsKubernetesDiagnosticObserver(DiagnosticLogLevel.Trace);
+                // ApplicationInsightsKubernetesDiagnosticSource.Instance.Observable.SubscribeWithAdapter(observer);
+
                 services.AddApplicationInsightsTelemetryWorkerService();
                 services.AddApplicationInsightsKubernetesEnricher();
                 services.AddHostedService<Worker>();
