5035eef4dd264d7c372074b2b32ea9cd48f336b6
Saar Shen <xiaomi7732@hotmail.com>
2022-06-10 14:50:06 -0700

    Remove unused using

--- src/ApplicationInsights.Kubernetes.HostingStartup/K8sInjection.cs
+++ src/ApplicationInsights.Kubernetes.HostingStartup/K8sInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ApplicationInsights.Kubernetes.HostingStartup
 {
