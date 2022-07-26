b0e71b7e200e53fb05b19822a9fe6b196d62a060
Saar Shen <xiaomi7732@hotmail.com>
2022-07-26 07:56:35 -0700

    Fixed PodExtensions tests

--- tests/UnitTests/PodExtensionsTests.cs
+++ tests/UnitTests/PodExtensionsTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.ApplicationInsights.Kubernetes.Entities;
+using k8s.Models;
 using Xunit;
 
 namespace Microsoft.ApplicationInsights.Kubernetes;
@@ -10,18 +10,18 @@ public class TestK8sPodExtensionMethods
     {
         string containerId = "b1bf9cd89b57ba86c20e17bfd474638110e489da784a5e388983294d94ae9fc4";
         string containerName = "testContainerName";
-        K8sPod pod = new K8sPod();
-        pod.Status = new K8sPodStatus()
+        V1Pod pod = new V1Pod();
+        pod.Status = new V1PodStatus()
         {
-            ContainerStatuses = new ContainerStatus[]{
-                new ContainerStatus(){
+            ContainerStatuses = new V1ContainerStatus[]{
+                new V1ContainerStatus(){
                     Name=containerName,
                     ContainerID = "docker://b1bf9cd89b57ba86c20e17bfd474638110e489da784a5e388983294d94ae9fc4"
                 },
             },
         };
 
-        ContainerStatus result = pod.GetContainerStatus(containerId);
+        V1ContainerStatus result = pod.GetContainerStatus(containerId);
         Assert.NotNull(result);
         Assert.Equal(containerName, result.Name);
     }
@@ -29,24 +29,24 @@ public class TestK8sPodExtensionMethods
     [Fact]
     public void GetContainerStatusShallReturnNullWhenContainerIdNullOrEmpty()
     {
-        K8sPod pod = new K8sPod();
-        pod.Status = new K8sPodStatus()
+        V1Pod pod = new V1Pod();
+        pod.Status = new V1PodStatus()
         {
-            ContainerStatuses = new ContainerStatus[]{
-                new ContainerStatus(){
+            ContainerStatuses = new V1ContainerStatus[]{
+                new V1ContainerStatus(){
                     Name="testContainerName",
                     ContainerID = "docker://b1bf9cd89b57ba86c20e17bfd474638110e489da784a5e388983294d94ae9fc4"
                 },
             },
         };
 
-        ContainerStatus result = pod.GetContainerStatus(null);
+        V1ContainerStatus result = pod.GetContainerStatus(null);
         Assert.Null(result);
 
-        ContainerStatus result2 = pod.GetContainerStatus(string.Empty);
+        V1ContainerStatus result2 = pod.GetContainerStatus(string.Empty);
         Assert.Null(result2);
 
-        ContainerStatus result3 = pod.GetContainerStatus("");
+        V1ContainerStatus result3 = pod.GetContainerStatus("");
         Assert.Null(result3);
     }
 }
