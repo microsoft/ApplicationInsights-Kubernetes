88c0e984575576f133e245f834dbbb989f8bbb16
Saar Shen <xiaomi7732@hotmail.com>
2022-08-26 14:59:59 -0700

    Guarantee K8sEnv is completed update before consumed by another thread

--- src/ApplicationInsights.Kubernetes/K8sEnvironmentHolder.cs
+++ src/ApplicationInsights.Kubernetes/K8sEnvironmentHolder.cs
@@ -3,8 +3,10 @@ namespace Microsoft.ApplicationInsights.Kubernetes;
 /// <summary>
 /// Provides a single point of access for K8sEnvironment object.
 /// </summary>
-internal sealed class K8sEnvironmentHolder
+internal sealed class K8sEnvironmentHolder : IK8sEnvironmentHolder
 {
+    private volatile IK8sEnvironment? _k8SEnvironment;
+
     private K8sEnvironmentHolder() { }
 
     public static K8sEnvironmentHolder Instance { get; } = new K8sEnvironmentHolder();
@@ -13,5 +15,15 @@ internal sealed class K8sEnvironmentHolder
     /// Gets or sets the kubernetes environment object.
     /// Returns null when the environment is info is not ready for consuming.
     /// </summary>
-    public IK8sEnvironment? K8sEnvironment { get; internal set; }
+    public IK8sEnvironment? K8sEnvironment
+    {
+        get
+        {
+            return _k8SEnvironment;
+        }
+        set
+        {
+            _k8SEnvironment = value;
+        }
+    }
 }
