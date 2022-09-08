f9d0ed41fec30f4edca654f13e6dc902f7c29dff
Saar Shen <xiaomi7732@hotmail.com>
2022-09-08 15:17:45 -0700

    Clean up lifetime of registered services

--- src/ApplicationInsights.Kubernetes/Extensions/KubernetesServiceCollectionBuilder.cs
+++ src/ApplicationInsights.Kubernetes/Extensions/KubernetesServiceCollectionBuilder.cs
@@ -80,11 +80,12 @@ namespace Microsoft.Extensions.DependencyInjection
         private static void RegisterCommonServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<ITelemetryKeyCache, TelemetryKeyCache>();
-            serviceCollection.AddSingleton<SDKVersionUtils>(p => SDKVersionUtils.Instance);
-            serviceCollection.AddSingleton<IK8sClientService>(p => K8sClientService.Instance);
+            serviceCollection.AddSingleton<SDKVersionUtils>(_ => SDKVersionUtils.Instance);
+            serviceCollection.AddSingleton<IK8sClientService>(_ => K8sClientService.Instance);
             serviceCollection.AddSingleton<IContainerIdHolder, ContainerIdHolder>();
-            serviceCollection.AddSingleton<IPodInfoManager, PodInfoManager>();
-            serviceCollection.AddSingleton<IContainerStatusManager, ContainerStatusManager>();
+
+            serviceCollection.AddScoped<IPodInfoManager, PodInfoManager>();
+            serviceCollection.AddScoped<IContainerStatusManager, ContainerStatusManager>();
         }
 
         /// <summary>
@@ -94,39 +95,39 @@ namespace Microsoft.Extensions.DependencyInjection
         protected virtual void RegisterSettingsProvider(IServiceCollection serviceCollection)
         {
             // This happens before any of the other IContainerIdProvider. See comments below for details.
-            serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IContainerIdProvider, EnvironmentVariableContainerIdProvider>());    // Environment variable - works for both Linux & Windows;
+            serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IContainerIdProvider, EnvironmentVariableContainerIdProvider>());    // Environment variable - works for both Linux & Windows;
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 // Matchers are dependencies of the container id providers.
-                serviceCollection.AddSingleton<CGroupV1Matcher>();
-                serviceCollection.AddSingleton<DockerEngineMountInfoMatcher>();
-                serviceCollection.AddSingleton<ContainerDMountInfoMatcher>();
+                serviceCollection.AddScoped<CGroupV1Matcher>();
+                serviceCollection.AddScoped<DockerEngineMountInfoMatcher>();
+                serviceCollection.AddScoped<ContainerDMountInfoMatcher>();
 
                 // Notes: pay attention to the order. Injecting uses the order of registering in this case.
                 // For example, EnvironmentVariableContainerIdProvider will take precedence, then CGroupContainerIdProvider on Linux.
-                serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IContainerIdProvider, CGroupContainerIdProvider>());                 // Then CGroupV1
-                serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IContainerIdProvider, ContainerDMountInfoContainerIdProvider>());    // Then ContainerD
-                serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IContainerIdProvider, DockerEngineMountInfoContainerIdProvider>());  // Then DockerEngine
+                serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IContainerIdProvider, CGroupContainerIdProvider>());                 // Then CGroupV1
+                serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IContainerIdProvider, ContainerDMountInfoContainerIdProvider>());    // Then ContainerD
+                serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IContainerIdProvider, DockerEngineMountInfoContainerIdProvider>());  // Then DockerEngine
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 // Notes: pay attention to the order. Injecting uses the order of registering in this case.
                 // For example, EnvironmentVariableContainerIdProvider will take precedence, then EmptyContainerIdProvider on Windows.
-                serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IContainerIdProvider, EmptyContainerIdProvider>());
+                serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IContainerIdProvider, EmptyContainerIdProvider>());
             }
             else
             {
                 _logger.LogError("Unsupported OS.");
             }
 
-            serviceCollection.TryAddSingleton<IContainerIdNormalizer, ContainerIdNormalizer>();
+            serviceCollection.TryAddScoped<IContainerIdNormalizer, ContainerIdNormalizer>();
 
             // Notes: pay attention to the order. Injecting uses the order of registering in this case.
             // For backward compatibility, $APPINSIGHTS_KUBERNETES_POD_NAME has been agreed upon to allow customize pod name with downward API.
-            serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IPodNameProvider, UserSetPodNameProvider>());
+            serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IPodNameProvider, UserSetPodNameProvider>());
             // $Hostname will be overwritten by Kubernetes to reveal pod name: https://kubernetes.io/docs/concepts/containers/container-environment/#container-information.
-            serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton<IPodNameProvider, HostNamePodNameProvider>());
+            serviceCollection.TryAddEnumerable(ServiceDescriptor.Scoped<IPodNameProvider, HostNamePodNameProvider>());
         }
 
         /// <summary>
