e3eb4de7cfe27b72ecfe8c02af0e1bab93520b35
Saar Shen <xiaomi7732@hotmail.com>
2023-01-26 09:59:51 -0800

    Application Insights for Kubernetes 6.0 (#334)
    * Upgrade to latest K8sClient
    
    * Update default version to 6.0 to align with targeted framework
    
    * Remove an unintended change

--- src/ApplicationInsights.Kubernetes/K8sClientService.cs
+++ src/ApplicationInsights.Kubernetes/K8sClientService.cs
@@ -49,22 +49,22 @@ internal sealed class K8sClientService : IDisposable, IK8sClientService
 
     public async Task<IEnumerable<V1Pod>> GetPodsAsync(CancellationToken cancellationToken)
     {
-        V1PodList? list = await _kubernetesClient.ListNamespacedPodAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
+        V1PodList? list = await _kubernetesClient.CoreV1.ListNamespacedPodAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
         return list.AsEnumerable();
     }
 
     public Task<V1Pod?> GetPodByNameAsync(string podName, CancellationToken cancellationToken)
-        => _kubernetesClient.ReadNamespacedPodAsync(podName, _namespace, cancellationToken: cancellationToken);
+        => _kubernetesClient.CoreV1.ReadNamespacedPodAsync(podName, _namespace, cancellationToken: cancellationToken);
 
     public async Task<IEnumerable<V1ReplicaSet>> GetReplicaSetsAsync(CancellationToken cancellationToken)
     {
-        V1ReplicaSetList? replicaSetList = await _kubernetesClient.ListNamespacedReplicaSetAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
+        V1ReplicaSetList? replicaSetList = await _kubernetesClient.AppsV1.ListNamespacedReplicaSetAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
         return replicaSetList.AsEnumerable();
     }
 
     public async Task<IEnumerable<V1Deployment>> GetDeploymentsAsync(CancellationToken cancellationToken)
     {
-        V1DeploymentList? deploymentList = await _kubernetesClient.ListNamespacedDeploymentAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
+        V1DeploymentList? deploymentList = await _kubernetesClient.AppsV1.ListNamespacedDeploymentAsync(_namespace, cancellationToken: cancellationToken).ConfigureAwait(false);
         return deploymentList.AsEnumerable();
     }
 
@@ -72,7 +72,7 @@ internal sealed class K8sClientService : IDisposable, IK8sClientService
     {
         try
         {
-            V1NodeList? nodeList = await _kubernetesClient.ListNodeAsync();
+            V1NodeList? nodeList = await _kubernetesClient.CoreV1.ListNodeAsync().ConfigureAwait(false);
             return nodeList.AsEnumerable();
         }
         catch (HttpOperationException ex) when (ex.Response.StatusCode == HttpStatusCode.Forbidden)
