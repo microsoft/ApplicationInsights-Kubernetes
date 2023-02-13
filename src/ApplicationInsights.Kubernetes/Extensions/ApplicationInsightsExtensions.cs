c41f74e23ac0808ff683c4900c99da7c82bfb0af
Saar Shen <xiaomi7732@hotmail.com>
2023-02-13 09:50:54 -0800

    Support In-Proc Azure Function in K8s (#337)
    * Support non-hostedservice environment
    
    * Remove unintentionally appended submodule
    
    * Renaming a parameter for better semantics
    
    Suggested by Karol, skip registering backend service sounds more like
    an implementation details while disableBackgroundService sounds more
    like a configuration.

--- src/ApplicationInsights.Kubernetes/Extensions/ApplicationInsightsExtensions.cs
+++ src/ApplicationInsights.Kubernetes/Extensions/ApplicationInsightsExtensions.cs
@@ -18,14 +18,18 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Enables Application Insights for Kubernetes on the Default TelemetryConfiguration in the dependency injection container with custom options.
         /// </summary>
         /// <param name="services">Collection of service descriptors.</param>
-        /// <param name="applyOptions">Action to customize the configuration of Application Insights for Kubernetes.</param>
         /// <param name="diagnosticLogLevel">Sets the diagnostics log levels for the enricher.</param>
+        /// <param name="disableBackgroundService">If true, the Application Insights enricher library will not use any background (hosted) services,
+        /// and Kubernetes information will not be fetched automatically. Hosted services are not allowed in some environments, e.g. Azure Function. 
+        /// For more information see https://github.com/Azure/azure-functions-host/issues/5447#issuecomment-575368316</param>
+        /// <param name="applyOptions">Action to customize the configuration of Application Insights for Kubernetes.</param>
         /// <param name="clusterCheck">Provides a custom implementation to check whether it is inside kubernetes cluster or not.</param>
         /// <returns>The service collection for chaining the next operation.</returns>
         public static IServiceCollection AddApplicationInsightsKubernetesEnricher(
             this IServiceCollection services,
-            Action<AppInsightsForKubernetesOptions>? applyOptions = default,
             LogLevel? diagnosticLogLevel = LogLevel.None,
+            bool disableBackgroundService = false,
+            Action<AppInsightsForKubernetesOptions>? applyOptions = default,
             IClusterEnvironmentCheck? clusterCheck = default)
         {
             diagnosticLogLevel ??= LogLevel.None;   // Default to None.
@@ -37,7 +41,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             if (!KubernetesTelemetryInitializerExists(services))
             {
-                services.ConfigureKubernetesTelemetryInitializer(applyOptions, clusterCheck);
+                services.ConfigureKubernetesTelemetryInitializer(applyOptions, clusterCheck, disableBackgroundService);
             }
             return services;
         }
@@ -48,7 +52,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static void StartApplicationInsightsKubernetesEnricher(this IServiceProvider serviceProvider)
         {
             IK8sInfoBootstrap? k8sInfoBootstrap = serviceProvider.GetService<IK8sInfoBootstrap>();
-            if(k8sInfoBootstrap is null)
+            if (k8sInfoBootstrap is null)
             {
                 _logger.LogInformation("No service registered by type {0}. Either not running in a Kubernetes cluster or `{1}()` wasn't called on the service collection.", nameof(IK8sInfoBootstrap), nameof(AddApplicationInsightsKubernetesEnricher));
                 return;
@@ -69,9 +73,10 @@ namespace Microsoft.Extensions.DependencyInjection
         internal static void ConfigureKubernetesTelemetryInitializer(
             this IServiceCollection services,
             Action<AppInsightsForKubernetesOptions>? overwriteOptions,
-            IClusterEnvironmentCheck? clusterCheck)
+            IClusterEnvironmentCheck? clusterCheck,
+            bool skipRegisterBackendService = false)
         {
-            IKubernetesServiceCollectionBuilder kubernetesServiceCollectionBuilder = new KubernetesServiceCollectionBuilder(overwriteOptions, clusterCheck);
+            IKubernetesServiceCollectionBuilder kubernetesServiceCollectionBuilder = new KubernetesServiceCollectionBuilder(overwriteOptions, clusterCheck, skipRegisterBackendService);
             _ = kubernetesServiceCollectionBuilder.RegisterServices(services);
         }
     }
